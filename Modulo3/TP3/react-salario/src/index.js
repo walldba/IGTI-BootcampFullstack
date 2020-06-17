36fa4fbbd4ca7bff8ef2a64b413b589721796215
wallace.jeronimo <linkin01>
2020-06-16 22:59:06 -0300

    Adicionando materialize

--- Modulo3/TP3/react-salario/src/index.js
+++ Modulo3/TP3/react-salario/src/index.js
@@ -1,6 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
+import "materialize-css/dist/css/materialize.min.css";
+import "./index.css";
 
 ReactDOM.render(
   <React.StrictMode>
